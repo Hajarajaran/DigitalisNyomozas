6dfda220f0c3273dc4891a630862ca8ad66ca41c
Rábai Miklós <rabaimiklos2001@gmail.com>
2026-01-29 14:33:50 +0100

    User osztály alapvető adatok kész

--- DigitalisNyomozas/User.cs
+++ DigitalisNyomozas/User.cs
@@ -8,5 +8,19 @@ namespace DigitalisNyomozas
 {
 	internal class User
 	{
+		private string nev;
+		private int id;
+		private string nyomozo;
+
+		public User(string nev, int id, string nyomozo)
+		{
+			this.nev = nev;
+			this.id = id;
+			this.nyomozo = nyomozo;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Id { get => id; set => id = value; }
+		public string Nyomozo { get => nyomozo; set => nyomozo = value; }
 	}
 }
