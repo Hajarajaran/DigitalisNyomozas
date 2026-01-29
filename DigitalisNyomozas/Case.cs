74d1dc86baf2d05f801b4466812fad5a248b3b84
Rábai Miklós <rabaimiklos2001@gmail.com>
2026-01-29 14:43:24 +0100

    Case osztály alapvető adatok kész

--- DigitalisNyomozas/Case.cs
+++ DigitalisNyomozas/Case.cs
@@ -8,5 +8,28 @@ namespace DigitalisNyomozas
 {
 	internal class Case
 	{
+		private string ugyId;
+		private string cim;
+		private string leiras;
+		private string allapot;
+		private List<Person> szemelyekLista;
+		private List<Evidence> bizonyitekokLista;
+
+		public Case(string ugyId, string cim, string leiras, string allapot, List<Person> szemelyekLista, List<Evidence> bizonyitekokLista)
+		{
+			this.ugyId = ugyId;
+			this.cim = cim;
+			this.leiras = leiras;
+			this.allapot = allapot;
+			this.szemelyekLista = szemelyekLista;
+			this.bizonyitekokLista = bizonyitekokLista;
+		}
+
+		public string UgyId { get => ugyId; set => ugyId = value; }
+		public string Cim { get => cim; set => cim = value; }
+		public string Leiras { get => leiras; set => leiras = value; }
+		public string Allapot { get => allapot; set => allapot = value; }
+		internal List<Person> SzemelyekLista { get => szemelyekLista; set => szemelyekLista = value; }
+		internal List<Evidence> BizonyitekokLista { get => bizonyitekokLista; set => bizonyitekokLista = value; }
 	}
 }
