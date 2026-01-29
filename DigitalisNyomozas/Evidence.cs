c4d9e120bd3308c8e746a6c7a1c1d47fd2b5dab0
Rábai Miklós <rabaimiklos2001@gmail.com>
2026-01-29 14:58:42 +0100

    Evidence osztály alapvető adatok kész

--- DigitalisNyomozas/Evidence.cs
+++ DigitalisNyomozas/Evidence.cs
@@ -8,5 +8,22 @@ namespace DigitalisNyomozas
 {
 	internal class Evidence
 	{
+		private string id;
+		private string tipus;
+		private string leiras;
+		private int megbizhatosagiErtek;
+
+		public Evidence(string id, string tipus, string leiras, int megbizhatosagiErtek)
+		{
+			this.id = id;
+			this.tipus = tipus;
+			this.leiras = leiras;
+			this.megbizhatosagiErtek = megbizhatosagiErtek;
+		}
+
+		public string Id { get => id; set => id = value; }
+		public string Tipus { get => tipus; set => tipus = value; }
+		public string Leiras { get => leiras; set => leiras = value; }
+		public int MegbizhatosagiErtek { get => megbizhatosagiErtek; set => megbizhatosagiErtek = value; }
 	}
 }
