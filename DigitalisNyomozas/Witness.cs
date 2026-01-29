111f27a0a4575447a27ee1b26154d969820a7ae8
Rábai Miklós <rabaimiklos2001@gmail.com>
2026-01-29 14:55:00 +0100

    Witness osztály alapvető adatok kész

--- DigitalisNyomozas/Witness.cs
+++ DigitalisNyomozas/Witness.cs
@@ -8,5 +8,19 @@ namespace DigitalisNyomozas
 {
 	internal class Witness
 	{
+		private Person szemely;
+		private string vallomasSzovege;
+		private string vallomasDatuma;
+
+		public Witness(Person szemely, string vallomasSzovege, string vallomasDatuma)
+		{
+			this.szemely = szemely;
+			this.vallomasSzovege = vallomasSzovege;
+			this.vallomasDatuma = vallomasDatuma;
+		}
+
+		public string VallomasSzovege { get => vallomasSzovege; set => vallomasSzovege = value; }
+		public string VallomasDatuma { get => vallomasDatuma; set => vallomasDatuma = value; }
+		internal Person Szemely { get => szemely; set => szemely = value; }
 	}
 }
